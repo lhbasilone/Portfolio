4d8602578a6b525d48751dd913c28711655efac7
lhbasilone <lucasbasilone@gmail.com>
2023-05-23 02:35:11 -0300

    23/05/23

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -21,21 +21,21 @@ const AboutMe = () => {
       <div id='about-me-section' className='about-me'>
           <div className='about-me-text'>
           <motion.h1
-            initial={{ x: -100, opacity: 0 }}
+            initial={{ x: 0, opacity: 0 }}
             whileInView={{ x: 0, opacity: 1 }}
             transition={{ duration: 1 }}
           >
           Sobre Mi
           </motion.h1>
           <motion.p
-            initial={{ x: -100, opacity: 0 }}
+            initial={{ x: 0, opacity: 0 }}
             whileInView={{ x: 0, opacity: 1 }}
             transition={{ duration: 1 }}
           >
             Me llamo Lucas Basilone, soy de Argentina, tengo 28 años y soy Developer web con conocimientos en JavaScript, HTML, CSS, ReactJS, NodeJS y varias tecnologías más como Python y MySQL.
           </motion.p>
           <motion.button className='button-neobrut' onClick={handleDownload}
-            initial={{ x: 100, opacity: 0 }}
+            initial={{ x: 0, opacity: 0 }}
             whileInView={{ x: 0, opacity: 1 }}
             transition={{ duration: 1 }}
           >
@@ -43,8 +43,8 @@ const AboutMe = () => {
           </motion.button>
           </div>
           <motion.img src={me} className='about-me-img'
-            initial={{ x: 100, opacity: 0 }}
-            whileInView={{ x: 0, opacity: 1 }}
+            initial={{ opacity: 0 }}
+            whileInView={{opacity: 1 }}
             transition={{ duration: 1 }}
           />
           <h1 className='about-me-title'>Sobre Mi</h1>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,13 +9,13 @@ const Footer = () => {
     <>
       <div className='footer' id='footer-section'>
         <aside>
-          <motion.h2 className='footer-title'
+          <h2 className='footer-title'
             initial={{x:-500}}
             whileInView={{ x: 0 }}
             transition={{ type: "spring", stiffness: 100 }}
           >¿TIENES UN PROYECTO? HABLEMOS
-          </motion.h2>
-            <motion.div className='footer-separate'
+          </h2>
+            <div className='footer-separate'
               initial={{x:500}}
               whileInView={{ x: 0 }}
               transition={{ type: "spring", stiffness: 100 }}
@@ -23,13 +23,13 @@ const Footer = () => {
               <p className='footer-text'>Estoy disponible para trabajar en sus increíbles proyectos. Un desarrollador front-end de Argentina apasionado por crear webs funcionales</p>        
               <p className='footer-follow'>Seguime</p>
               <div className='footer-button-div'>
-                <motion.a href="https://github.com/lhbasilone" className='footer-button'drag >
-                  <motion.img src={github} alt="" drag/>
-                </motion.a>
-                <motion.a href="https://www.instagram.com/lucsbasilone/" className='footer-button' drag><motion.img src={instagram} alt="" drag/></motion.a>
-                <motion.a href="https://www.linkedin.com/in/lucasbasilone" className='footer-button' drag><motion.img src={linkedin} alt="" drag/></motion.a>
+                <a href="https://github.com/lhbasilone" className='footer-button'drag >
+                  <img src={github} alt="" drag/>
+                </a>
+                <a href="https://www.instagram.com/lucsbasilone/" className='footer-button' drag><motion.img src={instagram} alt="" drag/></a>
+                <a href="https://www.linkedin.com/in/lucasbasilone" className='footer-button' drag><motion.img src={linkedin} alt="" drag/></a>
               </div>
-            </motion.div>
+            </div>
         </aside>
       </div>
     </>
